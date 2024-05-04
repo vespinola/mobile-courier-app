d5c8626bfd3de67a1993a4570648df436224144d
Vladimir Espinola <vladimir.espinola@gmail.com>
2024-05-04 14:56:34 -0400

    added initial project files

---
+++ mobile-courier-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile-courier-app
+//
+//  Created by Vladimir Espinola on 2024-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile-courier-app/mobile_courier_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_courier_appApp.swift
+//  mobile-courier-app
+//
+//  Created by Vladimir Espinola on 2024-05-04.
+//
+
+import SwiftUI
+
+@main
+struct mobile_courier_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
